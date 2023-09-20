a858068b8772b48be4c38cf03dfc64a435326b2e
myway00 <junior0614@naver.com>
2023-09-21 00:49:04 +0900

    ADD : Authority check interceptor

--- mysite03/src/main/java/com/poscodx/mysite/security/AuthInterceptor.java
+++ mysite03/src/main/java/com/poscodx/mysite/security/AuthInterceptor.java
@@ -10,50 +10,68 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import com.poscodx.mysite.vo.UserVo;
 
 public class AuthInterceptor implements HandlerInterceptor {
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		//1. handler 종류 확인
-		if(!(handler instanceof HandlerMethod)) {
-			// DefaultServletHanlder가 처리하는 경우(정적 자원, /assets/**)
-			return true;
-		}
-		
-		//2. casting
-		HandlerMethod handlerMethod = (HandlerMethod)handler;
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        //1. handler 종류 확인
+        if (!(handler instanceof HandlerMethod)) {
+            // DefaultServletHanlder가 처리하는 경우(정적 자원, /assets/**)
+            return true;
+        }
 
-		//3-1. Handler Method의 @Auth 가져오기
-		Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
-		
-		//3-2. Handler Method의 @Auth가 없는 경우, Type(Class)의 @Auth 가져오기
-		if(auth == null) {
-			//handlerMethod.
-		}
-		
-		//4. @Auth 가 없는 경우
-		if(auth == null) {
-			return true;
-		}
-		
-		//5. @Auth 가 붙어 있는 경우, 인증(Authenfication) 여부 확인
-		HttpSession session = request.getSession();
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
+        //2. casting
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
 
-		if(authUser == null) {
-			response.sendRedirect(request.getContextPath() + "/user/login");
-			return false;
-		}
+        //3-1. Handler Method의 @Auth 가져오기
+        // 메소드가 지정하는 권한
+        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
 
-		//6. 권한(Authorization) 체크를 위해서 @Auth의 Role 가져오기("USER", "ADMIN")
-		String role = auth.Role();
-		
-		String authUserRole = authUser.getRole();
+        //3-2. Handler Method의 @Auth가 없는 경우, Type(Class)의 @Auth 가져오기
+        if (auth == null) {
+            //handlerMethod.
+            auth = handlerMethod.getBeanType().getAnnotation(Auth.class);
+        }
 
-		
-		
-		
-		//6. 인증 확인!!!
-		return true;
-	}
+        //4. @Auth 가 없는 경우
+        // 따로 권한이 없는 메소드와 클래스이므로 허용
+        if (auth == null) {
+            return true;
+        }
+
+        //5. @Auth 가 붙어 있는 경우, 인증(Authenfication) 여부 확인
+        // 5-1. 세션이 없는 경우 -> 로그인
+        HttpSession session = request.getSession();
+        if (session == null) {
+            response.sendRedirect(request.getContextPath() + "/user/login");
+            return false;
+        }
+
+        // 5-2. 세션이 있는 경우
+        // 유효한 세션 얻기 위해 -> 로그인
+        UserVo authUser = (UserVo) session.getAttribute("authUser");
+        if (authUser == null) {
+            response.sendRedirect(request.getContextPath() + "/user/login");
+            return false;
+        }
+
+        //6. 권한(Authorization) 체크를 위해서 @Auth의 Role 가져오기("USER", "ADMIN")
+        // 메소드가 허용하는 권한
+        String role = auth.Role();
+        if ("USER".equals(role)) {
+            return true;
+        }
+
+        // 현재 유저의 권한
+        String authUserRole = authUser.getRole()
+        if ("ADMIN".equals(authUserRole) == false) {
+            response.sendRedirect(request.getContextPath());
+            return false;
+        }
+
+        //6. 인증 확인!!!
+        //	@Auth의 role: "ADMIN"
+        //	authUser의 role : "ADMIN"
+        return true;
+    }
 
 }
